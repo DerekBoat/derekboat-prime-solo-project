d9d7ec46bc762d3fef9689e68294feab4c4c2553
Derek Boat <derek.boat@gmail.com>
2020-03-26 13:11:06 -0500

    fixed up database

--- database.sql
+++ database.sql
@@ -9,7 +9,8 @@
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "admin" BOOLEAN default false
 );
 
 CREATE TABLE "posts" (
